4b0e061b138cff4c99c74bd1610f59393d2b6ba3
thepawan33 <thepawan33@gmail.com>
2024-09-28 16:33:31 +0530

    testing

--- utils/otp.js
+++ utils/otp.js
@@ -3,8 +3,8 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.Password,
