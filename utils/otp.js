db4c85245738160fb6519f0abf9e42d64d7c52c2
thepawan33 <thepawan33@gmail.com>
2024-09-29 12:30:31 +0530

    check credential

--- utils/otp.js
+++ utils/otp.js
@@ -9,9 +9,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL,
     pass: process.env.Password,
   },
-  logger: true,
-  debug: true,
 });
+console.log("email :", process.env.EMAIL);
+console.log("email :", process.env.PASSWORd);
 
 const generateSecret = () => {
   let secret = speakeasy.generateSecret({ length: 4 });
